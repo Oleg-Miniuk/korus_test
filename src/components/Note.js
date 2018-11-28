0b1f3c2b129eecfb8b0725b28a1f6f05a6ef1fce
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-11-28 14:50:32 +0300

    Note Compenent init

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,3 +1,16 @@
-const Note = () => {};
+import React, { Component } from 'react';
+
+class Note extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <input />
+        <input />
+      </div>
+    );
+  }
+}
 
 export default Note;
